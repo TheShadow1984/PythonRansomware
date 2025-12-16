d4c10d28d3143b018474e801592a7b6c2a5afcf7
Shadow <shadows.mail@icloud.com>
2025-12-16 19:38:14 +0100

    Refactor image loading to use resource path

--- scr/blue_screen.py
+++ scr/blue_screen.py
@@ -1,11 +1,18 @@
 import tkinter as tk
+import sys
+import os
 from PIL import Image, ImageTk
+
+def resource_path(path):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, path)
+    return path
 
 root = tk.Tk()
 
 # Vollbild ohne Taskleiste
 root.attributes("-fullscreen", True)
-root.attributes('-topmost', True)
+root.attributes("-topmost", True)
 root.overrideredirect(True)
 
 # Bildschirmgröße ermitteln
@@ -13,7 +20,7 @@ screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
 # Bild laden und an Bildschirmgröße anpassen
-img = Image.open("bsodmaker.png")
+img = Image.open(resource_path("bsodmaker.png"))
 img = img.resize((screen_width, screen_height))
 photo = ImageTk.PhotoImage(img)
 
@@ -21,10 +28,11 @@ label = tk.Label(root, image=photo)
 label.pack()
 
 def disable_close_event(event):
-        return "break" 
-# ESC zum Beenden
+    return "break"
+
+# Tasten
 root.bind("<Control-Shift-KeyPress-F>", lambda e: root.destroy())
 root.bind("<Control-w>", disable_close_event)
 root.bind("<Alt-F4>", disable_close_event)
-root.mainloop()
 
+root.mainloop()
