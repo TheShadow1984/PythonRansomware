ba910eacf9fcbedca36343ade742bd6dc14023ca
Redeagle <redeagle.2010.1@gmail.com>
2025-12-16 17:58:49 +0100

    joa

---
+++ scr/blue_screen.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+root = tk.Tk()
+
+# Vollbild ohne Taskleiste
+root.attributes("-fullscreen", True)
+root.attributes('-topmost', True)
+root.overrideredirect(True)
+
+# Bildschirmgröße ermitteln
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Bild laden und an Bildschirmgröße anpassen
+img = Image.open("bsodmaker.png")
+img = img.resize((screen_width, screen_height))
+photo = ImageTk.PhotoImage(img)
+
+label = tk.Label(root, image=photo)
+label.pack()
+
+def disable_close_event(event):
+        return "break" 
+# ESC zum Beenden
+root.bind("<Control-Shift-KeyPress-F>", lambda e: root.destroy())
+root.bind("<Control-w>", disable_close_event)
+root.bind("<Alt-F4>", disable_close_event)
+root.mainloop()
+
